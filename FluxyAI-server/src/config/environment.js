2e63ec681b4392433049b17245c5b9fbac82a166
Kaif1119 <mohdkaifsiddiqui1196@gmail.com>
2026-05-29 18:12:01 +0530

    Add .gitignore file and update package.json and environment.js formatting

--- FluxyAI-server/src/config/environment.js
+++ FluxyAI-server/src/config/environment.js
@@ -1,17 +1,15 @@
 import dotenv from "dotenv/config";
 
-const Secrets = [
-    "PORT",
-    "MONGO_URI"
-]
+const Secrets = ["PORT", "MONGO_URI"];
 
 const missing = Secrets.filter((secret) => {
-    return !process.env[secret];
-})
+  return !process.env[secret];
+});
 
 if (missing.length > 0) {
-    throw new Error(`Missing required environment variables: ${missing.join(", ")}`);
+  throw new Error(
+    `Missing required environment variables: ${missing.join(", ")}`,
+  );
 }
-
 
 export const { PORT, MONGO_URI } = process.env;
